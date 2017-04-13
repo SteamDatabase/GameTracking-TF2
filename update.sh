48d2a1ddbb1a6167153f2bac00b5b9aa6c4aa9f3
Wendy <freeman@steamdb.info>
2017-04-13 20:42:46 +0000

    3833195 | 1 files | M  update.sh

--- update.sh
+++ update.sh
@@ -10,19 +10,7 @@ ProcessVPK
 
 mono ../.support/SourceDecompiler/Decompiler.exe -i "tf/tf2_misc_dir.vpk" -o "tf/tf2_misc_dir/"
 
-echo "Fixing UCS-2"
-
-while IFS= read -r -d '' file
-do
-        if ! file --mime "$file" | grep "charset=utf-16le"
-        then
-                continue
-        fi
-
-        temp_file=$(mktemp)
-        iconv -t UTF-8 -f UCS-2 -o "$temp_file" "$file" &&
-        mv -f "$temp_file" "$file"
-done <   <(find . -name "*.txt" -type f -print0)
+FixUCS2
 
 if ! [[ $1 = "no-git" ]]; then
 	CreateCommit "$(grep "PatchVersion=" tf/steam.inf | grep -o '[0-9\.]*')"
