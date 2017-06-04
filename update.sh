08a93886dd7c5f26e37943293ce3068e5a8b34e8
Wendy <freeman@steamdb.info>
2017-06-04 22:09:21 +0000

    3990974 | 676 files | A  hl2/hl2_misc_dir/expressions/phonemes.txt, A  hl2/hl2_misc_dir/expressions/phonemes_strong.txt, A  hl2/hl2_misc_dir/expressions/phonemes_weak.txt, A  hl2/hl2_misc_dir/expressions/random.txt, A  hl2/hl2_misc_dir/expressions/randomalert.txt, A  hl2/hl2_misc_dir/materials/effects/blueblackflash.txt, A  hl2/hl2_misc_dir/materials/sprites/tp_beam001.txt, A  hl2/hl2_misc_dir/particles/particles_manifest.txt, A  hl2/hl2_misc_dir/resource/askconnectpanel.res, A  hl2/hl2_misc_dir/resource/baseactioncdtrackstartdialog.res, A  hl2/hl2_misc_dir/resource/baseactionpausedialog.res, A  hl2/hl2_misc_dir/resource/baseactionplaybackratedialog.res, A  hl2/hl2_misc_dir/resource/baseactionplaycommandsdialog.res, A  hl2/hl2_misc_dir/resource/baseactionplaysoundstartdialog.res, A  hl2/hl2_misc_dir/resource/baseactionscreenfadestartdialog.res, A  hl2/hl2_misc_dir/resource/baseactionskipaheaddialog.res, A  hl2/hl2_misc_dir/resource/baseactionstopplaybackdialog.res, A  hl2/hl2_misc_dir/resource/baseactiontextmessagestartdialog.res,

--- update.sh
+++ update.sh
@@ -7,11 +7,6 @@ echo "Processing TF2..."
 
 ProcessDepot ".dylib"
 ProcessVPK
-
-mono ../.support/SourceDecompiler/Decompiler.exe -i "tf/tf2_misc_dir.vpk" -o "tf/tf2_misc_dir/"
-
 FixUCS2
 
-if ! [[ $1 = "no-git" ]]; then
-	CreateCommit "$(grep "PatchVersion=" tf/steam.inf | grep -o '[0-9\.]*')"
-fi
+CreateCommit "$(grep "PatchVersion=" tf/steam.inf | grep -o '[0-9\.]*')"
