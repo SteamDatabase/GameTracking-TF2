6f11723b8eebcc5739082a900f7d661a73434b79
Wendy <freeman@steamdb.info>
2020-11-08 11:24:06 +0000

    6186971 | 3 files | M  .gitignore,  M update.sh, ?? tf/maps/
    https://steamdb.info/patchnotes/5789575/

--- update.sh
+++ update.sh
@@ -7,6 +7,12 @@ echo "Processing TF2..."
 
 ProcessDepot ".dylib"
 ProcessVPK
+
+while IFS= read -r -d '' file
+do
+	/home/steamdb/EntityLumpDumper/EntityLumpDumper/bin/Release/EntityLumpDumper "$file"
+done <   <(find "tf/maps/" -type f -name "*.bsp" -print0)
+
 FixUCS2
 
 CreateCommit "$(grep "PatchVersion=" tf/steam.inf | grep -o '[0-9\.]*')" "$1"
