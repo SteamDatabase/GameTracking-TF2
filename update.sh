e06e1fd0b98a14c353d3c94f803ba99bba38fb2b
Wendy <freeman@steamdb.info>
2022-03-14 17:47:40 +0000

    6986594 | 80 files | M update.sh, ?? tf/tf2_misc_dir/scripts/playerclasses/, ?? tf/tf2_misc_dir/scripts/tf_weapon_bat.txt, ?? tf/tf2_misc_dir/scripts/tf_weapon_bat_fish.txt, ?? tf/tf2_misc_dir/scripts/tf_weapon_bat_giftwrap.txt, ?? tf/tf2_misc_dir/scripts/tf_weapon_bat_wood.txt, ?? tf/tf2_misc_dir/scripts/tf_weapon_bonesaw.txt, ?? tf/tf2_misc_dir/scripts/tf_weapon_bottle.txt, ?? tf/tf2_misc_dir/scripts/tf_weapon_breakable_sign.txt, ?? tf/tf2_misc_dir/scripts/tf_weapon_buff_item.txt, ?? tf/tf2_misc_dir/scripts/tf_weapon_builder.txt, ?? tf/tf2_misc_dir/scripts/tf_weapon_cannon.txt, ?? tf/tf2_misc_dir/scripts/tf_weapon_charged_smg.txt, ?? tf/tf2_misc_dir/scripts/tf_weapon_cleaver.txt, ?? tf/tf2_misc_dir/scripts/tf_weapon_club.txt, ?? tf/tf2_misc_dir/scripts/tf_weapon_compound_bow.txt, ?? tf/tf2_misc_dir/scripts/tf_weapon_crossbow.txt, ?? tf/tf2_misc_dir/scripts/tf_weapon_drg_pomson.txt, ?? tf/tf2_misc_dir/scripts/tf_weapon_fireaxe.txt, ?? tf/tf2_misc_dir/scripts/tf_weapon_fists.txt, ?? tf/tf2_misc_dir/scripts/tf_weapon_flamethrower.

--- update.sh
+++ update.sh
@@ -10,11 +10,14 @@ ProcessVPK
 
 while IFS= read -r -d '' file
 do
+	# https://github.com/xPaw/EntityLumpDumper
 	/home/steamdb/EntityLumpDumper/EntityLumpDumper/bin/Release/linux-x64/publish/EntityLumpDumper "$file"
 done <   <(find "tf/maps/" -type f -name "*.bsp" -print0)
 
 while IFS= read -r -d '' file
 do
+	# https://github.com/foobarhl/vice_standalone
+	# build: g++ -o bin/vice src/*.cpp
 	/home/steamdb/vice_standalone/bin/vice -d -x .txt -k E2NcUkG2 "$file"
 done <   <(find "tf/tf2_misc_dir/scripts/playerclasses/" "tf/tf2_misc_dir/scripts/" -maxdepth 1 -type f -name "*.ctx" -print0)
 
