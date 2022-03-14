dd8fa32d6bf41e79ef5753dfdeac0a13cccfedff
mastercoms <mastercoms@tuta.io>
2022-03-14 13:33:00 -0400

    decrypt ICE encrypted ctx files
    Requires https://github.com/foobarhl/vice_standalone
    (licensed under Source SDK: https://github.com/ValveSoftware/source-sdk-2013/blob/master/LICENSE)

--- update.sh
+++ update.sh
@@ -13,6 +13,11 @@ do
 	/home/steamdb/EntityLumpDumper/EntityLumpDumper/bin/Release/linux-x64/publish/EntityLumpDumper "$file"
 done <   <(find "tf/maps/" -type f -name "*.bsp" -print0)
 
+while IFS= read -r -d '' file
+do
+	/home/steamdb/vice_standalone/bin/vice -d -x .txt -k E2NcUkG2 "$file"
+done <   <(find "tf/tf2_misc_dir/scripts/playerclasses/" "tf/tf2_misc_dir/scripts/" -maxdepth 1 -type f -name "*.ctx" -print0)
+
 FixUCS2
 
 CreateCommit "$(grep "PatchVersion=" tf/steam.inf | grep -o '[0-9\.]*')" "$1"
